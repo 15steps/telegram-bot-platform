292a1505eaad0746dc47479a02a37c8094292755
Wellington Felix <wfmf@cin.ufpe.br>
2019-12-25 17:48:43 -0300

    Add multiproject build

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.2.RELEASE"
+	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	kotlin("jvm") version "1.3.61"
+	kotlin("plugin.spring") version "1.3.61"
+}
+
+group = "cf.moviebot"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+allprojects {
+	repositories {
+		mavenCentral()
+	}
+}
+
+subprojects {
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+	dependencies {
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		testImplementation("org.springframework.boot:spring-boot-starter-test") {
+			exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+		}
+	}
+	
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "1.8"
+		}
+	}
+}
+
+
+tasks.withType<Wrapper> {
+	gradleVersion = "5.5"
+}
