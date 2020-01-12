6e229d62dc9c472cfef3e57a1c6cdfff161f0d66
Wellington Felix <wfmf@cin.ufpe.br>
2020-01-12 12:48:35 -0300

    Fix update bean

--- webhook-handler/src/main/kotlin/cf/moviebot/domain/Update.kt
+++ webhook-handler/src/main/kotlin/cf/moviebot/domain/Update.kt
@@ -2,9 +2,9 @@ package cf.moviebot.domain
 
 data class Update(
     val updateId: Long,
-    val message: Message,
-    val editedMessage: Message,
-    val channelPost: Message,
-    val editedChannelPost: Message,
-    val inlineQuery: InlineQuery
+    val message: Message?,
+    val editedMessage: Message?,
+    val channelPost: Message?,
+    val editedChannelPost: Message?,
+    val inlineQuery: InlineQuery?
 )
